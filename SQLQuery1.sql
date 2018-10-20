6de6c838fbc100cf1a5caf90bbe27a3c9b2f78aa
Corey Lutrick <coreylutrick@gmail.com>
2018-10-20 10:02:51 -0700

    finished the 7th query

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -24,6 +24,27 @@ select *
 from Employee 
 where Title = 'Sales Support Agent'
 
+select BillingCountry 
+from Invoice
+Group by BillingCountry
 
+select 
+	FullName = e.FirstName + ' ' + e.LastName
+from Invoice as i,
+	Customer as c,
+	Employee as e
+where c.SupportRepId = e.EmployeeId 
+	and i.CustomerId = c.CustomerId
+group by e.FirstName + ' ' + e.LastName;
 
-
+select 
+	i.Total,
+	CustomerFullName = c.FirstName + ' ' + c.LastName,
+	EmployeeFullName = e.FirstName + ' ' + e.LastName,
+	c.Country
+from invoice as i,
+	customer as c,
+	employee as e
+where c.CustomerId = i.CustomerId
+	and e.EmployeeId = c.SupportRepId
+	
